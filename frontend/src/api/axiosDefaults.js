5b9fdbfc6475c3e5dfa86e29f07555a31e94319a
Brionconroy <razvanbrion@gmail.com>
2024-02-06 17:14:14 +0000

    Change to the react porject

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-2023-6c240fdbe670.herokuapp.com/"
+// axios.defaults.baseURL = "https://drf-api-2023-6c240fdbe670.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
 
